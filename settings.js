abad13163b91b2a929dc828ea87f95de048e6066
gifteddevsmd <babydave081@gmail.com>
2025-06-25 03:14:26 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,63 +1,65 @@
 //base by DGXeon
-//recode by GIDDY TENNOR 
-//YouTube: @GIDDYTENNOR
-
+//recode by Dave
+//YouTube: @Dave
 
 const fs = require('fs')
 const chalk = require('chalk')
-if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
+if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname + '/.env' })
 
+global.SESSION_ID = process.env.SESSION_ID || 'Bellah~2z40RA7T#VRneuCjwFxSYHupVJSJHZDNmNDE_THLCNxjVd9jGWuw'
 
-global.SESSION_ID = process.env.SESSION_ID || 'Bellah~2z40RA7T#VRneuCjwFxSYHupVJSJHZDNmNDE_THLCNxjVd9jGWuw' 
-//owmner v card
-global.ytname = "YT: GIDDYTENNOR" //ur yt chanel name
-global.socialm = "IG: @GIDDYTENNOR" //ur github or insta name
-global.location = "Kenya" //ur location
+// Owner vCard
+global.ytname = "YT: Dave" // your YouTube channel
+global.socialm = "IG: @gifted_dave" // your Instagram or GitHub
+global.location = "Kenya" // your location
 
-//new
-global.botname = process.env.BOT_NAME ||'𝐁𝐞𝐥𝐥𝐚𝐡 𝐗𝐦𝐝' //enter your  bot name here
-global.ownernumber = process.env.OWNER_NUMBER ||'254104245659' //ur owner number
-global.ownername = '© TENNOMODZ' //ur owner name
-global.websitex = "https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
-global.wagc = "https://chat.whatsapp.com/Hs0AwkOaFzbGi5sjicdeTR" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
+// Bot Info
+global.botname = process.env.BOT_NAME || '𝐃𝐀𝐕𝐄 𝐗𝐌𝐃' // bot name
+global.ownernumber = process.env.OWNER_NUMBER || '254104245659' // owner number
+global.ownername = '© Dave' // owner name
+global.websitex = "https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D" // channel link
+global.wagc = "https://chat.whatsapp.com/Hs0AwkOaFzbGi5sjicdeTR" // WhatsApp group
 global.themeemoji = '🪀'
-global.wm = "GIDDY TENNOR"
-global.botscript = 'https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D' //'https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES' //script link
-global.packname = process.env.PACK_NAME ||"BELLAH XMD" //enter your stickers author name here
-global.author = "Giddy-Tennor"
-global.creator = "254703726139@s.whatsapp.net"
-global.xprefix = process.env.PREFIX ||'.'
-global.premium = ["254703726139"] // Premium User
+global.wm = "Dave"
+global.botscript = 'https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D' // bot script/channel link
+global.packname = process.env.PACK_NAME || "DAVE-XMD" // sticker pack name
+global.author = "Dave"
+global.creator = "254104260236@s.whatsapp.net"
+global.xprefix = process.env.PREFIX || '.'
+global.premium = ["254104260236"] // Premium users
 global.hituet = 0
 
-//bot sett
-global.typemenu = process.env.TYPE_MENU || 'v2' // menu type 'v1' => 'v8' 
-global.typereply = process.env.TYPE_REPLY ||'v1' // reply type 'v1' => 'v3'
-global.autoblocknumber = process.env.AUTOBLOCK_NUMBER || '263,234' //set autoblock country code
-global.antiforeignnumber = process.env.ANTIFOREIGN_NUMBER || '' //set anti foreign number country code
-global.welcome = process.env.WELCOME || 'false' //welcome/left in groups
-global.anticall = process.env.ANTI_CALL || 'false' //bot blocks user when called
-global.autoswview = process.env.AUTOSW_VIEW || 'true' //auto status/story view
-global.adminevent = true //show promote/demote message
-global.groupevent = true //show update messages in group chat
-//msg
+// Bot Settings
+global.typemenu = process.env.TYPE_MENU || 'v2'
+global.typereply = process.env.TYPE_REPLY || 'v1'
+global.autoblocknumber = process.env.AUTOBLOCK_NUMBER || '263,234'
+global.antiforeignnumber = process.env.ANTIFOREIGN_NUMBER || ''
+global.welcome = process.env.WELCOME || 'false'
+global.anticall = process.env.ANTI_CALL || 'false'
+global.autoswview = process.env.AUTOSW_VIEW || 'true'
+global.adminevent = true
+global.groupevent = true
+
+// Message Settings
 global.autorecord = process.env.AUTO_RECORD || 'false'
 global.autorecordtype = process.env.AUTO_RECORDTYPE || 'false'
 
 global.mess = {
-	limit: 'Your limit is up <\>',
+	limit: 'Your limit is up <\\>',
 	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
     done: 'Done ✓',
     error: 'Error !',
-    success: 'Succes •'
+    success: 'Success •'
 }
-//thumbnail
-global.thumb = fs.readFileSync('./XMEDIA/theme/bellah.jpg')
 
+// Thumbnail/Sticker Logo
+global.thumb = fs.readFileSync('./XMEDIA/theme/davexmd.jpg') // ← use your uploaded logo image
+
+// File Watcher for Auto-Reload
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
     fs.unwatchFile(file)
-    console.log(chalk.redBright(`Update'${__filename}'`))
+    console.log(chalk.redBright(`Update '${__filename}'`))
     delete require.cache[file]
     require(file)
 })
